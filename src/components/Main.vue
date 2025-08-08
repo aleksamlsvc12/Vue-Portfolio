732ab376550fab7d837794559b95c81d88bbc8ca
Aleksa <aleksamlsvc12@gmail.com>
2025-08-08 11:29:50 +0200

    main.vue responsive design fixed

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -21,9 +21,9 @@
 <template>
     <div class="flex-grow p-0 lg:p-8 flex justify-around items-center transition-all duration-700 flex-col-reverse md:flex-row">
       <div class="w-full flex flex-col animate-[slideLeftToRight_1s_ease-in-out] items-center md:items-start">
-        <div class="w-fit">
+        <div class="w-[80%]">
           <p class="mb-1 font-extrabold text-xs md:text-xl lg:text-2xl dark:text-white whitespace-nowrap" v-html="data.title[lang]"></p>
-          <p class="mb-4 dark:text-white text-[10px] lg:text-sm text-justify" v-html="data.description[lang]"></p>
+          <p class="mb-6 dark:text-white text-[10px] lg:text-sm text-justify" v-html="data.description[lang]"></p>
         </div>
         <button class="bg-purple-700 text-xs w-32 h-8 lg:w-40 lg:h-10 rounded-3xl flex justify-center items-center shadow-gray-400 shadow-md hover:shadow-purple-400 hover:bg-purple-600 transition-all" v-if="lang==='english'" @click="downloadCvEng">
           <i class="pi pi-file text-white text-xs md:text-sm mr-2"></i>
