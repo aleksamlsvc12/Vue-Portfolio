f53a1c2750b2a93d777933e8f73d21e76252afa9
Aleksa <aleksamlsvc12@gmail.com>
2025-10-24 11:14:05 +0200

    Only english version cv available

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,12 @@
 <script setup>
   import profileImage from '@/assets/images/profile.jpg'
-  import cvEng from '@/assets/docs/cv-aleksa-milosevic-english.pdf';
-  import cvSer from '@/assets/docs/cv-aleksa-milosevic-serbian.pdf';
+  import cv from '@/assets/docs/cv-aleksa-milosevic.pdf'
 
-  function downloadCvEng() {
-    window.open(cvEng, '_blank');
+  function downloadCv() {
+    window.open(cv, '_blank');
   }
 
-  function downloadCvSer() {
-    window.open(cvSer, '_blank');
-  }
+
 
   defineProps({
     data: Object,
@@ -22,18 +19,15 @@
     <div class="flex-grow p-0 lg:p-8 flex justify-around items-center transition-all duration-700 flex-col-reverse md:flex-row">
       <div class="w-full flex flex-col animate-[slideLeftToRight_1s_ease-in-out] items-center md:items-start">
         <div class="w-[80%]">
-          <p class="mb-1 font-extrabold text-xs md:text-xl lg:text-2xl dark:text-white whitespace-nowrap" v-html="data.title[lang]"></p>
+          <p class="mb-2 font-extrabold text-xs md:text-xl lg:text-2xl dark:text-white" v-html="data.title[lang]"></p>
           <p class="mb-6 dark:text-white text-[10px] lg:text-sm text-justify" v-html="data.description[lang]"></p>
         </div>
-        <button class="bg-purple-700 text-xs w-32 h-8 lg:w-40 lg:h-10 rounded-3xl flex justify-center items-center shadow-gray-400 shadow-md hover:shadow-purple-400 hover:bg-purple-600 transition-all" v-if="lang==='english'" @click="downloadCvEng">
+        <button class="bg-purple-700 text-xs w-32 h-8 lg:w-40 lg:h-10 rounded-3xl flex justify-center items-center shadow-gray-400 shadow-md hover:shadow-purple-400 hover:bg-purple-600 transition-all" @click="downloadCv">
           <i class="pi pi-file text-white text-xs md:text-sm mr-2"></i>
           <p class="text-white text-xs lg:text-sm">{{data.button[lang]}}</p>
         </button>
 
-        <button class="bg-purple-700 w-32 h-8 lg:w-40 lg:h-10 rounded-3xl flex justify-center items-center shadow-gray-400 shadow-md hover:shadow-purple-400 hover:bg-purple-600 transition-all" v-else @click="downloadCvSer">
-          <i class="pi pi-file text-white text-sm mr-2"></i>
-          <p class="text-white text-xs lg:text-sm">{{data.button[lang]}}</p>
-        </button>
+
       </div>
 
       <div class="w-full flex justify-center md:justify-end animate-[slideRightToLeft_1s_ease-in-out] mb-8 mt-8">
